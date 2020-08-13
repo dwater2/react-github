cc644096e2b48465cc3f647c1bfed6728ff9f725
Donizete Waterkemper <dwater2@gmail.com>
2020-08-13 20:50:33 -0300

    Integration with the Github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,60 +1,96 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logoGithub from '../../assets/logo-github.svg';
-import { Title, Form, Repositories } from './styles';
-import Repository from '../Repository';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError ] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+    
+    if(storagedRepositories){
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+  
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handlerAddRepossitory(event: FormEvent<HTMLFormElement>): Promise<void> {
+    //Adição de um novo repositório
+    //Consumir API do Github
+    //Salvar o repositório no novo estado
+    event.preventDefault();
+
+    if(!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoGithub} alt="Github Explorer"/>
       <Title>Explore respositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handlerAddRepossitory}>
+        <input 
+          placeholder="Digite o nome do repositório" 
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      { inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/10601064?s=400&u=7720fedbd2d6269720abe262f42ac194a7f11c27&v=4"
-            alt="Donizete Waterkemper"
-          />
-          <div>
-            <strong>dwater2/front-end-com-reactjs</strong>
-            <p>Aplicação de front-end desenvolvimento com REACTJS.</p>
-          </div>
+        {
+          repositories.map(repository => (
+            <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
 
-          <FiChevronRight size={20}/>
-        </a>
+              <FiChevronRight size={20}/>
+            </Link>
+          ))}
 
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/10601064?s=400&u=7720fedbd2d6269720abe262f42ac194a7f11c27&v=4"
-            alt="Donizete Waterkemper"
-          />
-          <div>
-            <strong>dwater2/front-end-com-reactjs</strong>
-            <p>Aplicação de front-end desenvolvimento com REACTJS.</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/10601064?s=400&u=7720fedbd2d6269720abe262f42ac194a7f11c27&v=4"
-            alt="Donizete Waterkemper"
-          />
-          <div>
-            <strong>dwater2/front-end-com-reactjs</strong>
-            <p>Aplicação de front-end desenvolvimento com REACTJS.</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
       </Repositories>
     </>
   );  
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 40px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -22,6 +26,12 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border-right: 0;
+
+    ${(props) =>
+      props.hasError && css`
+        border-color: #c53030;
+    `}
 
     &::placeholder{
       color: #a8a8b3;
@@ -75,7 +85,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
@@ -95,4 +106,10 @@ export const Repositories = styled.div`
     }
 
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
